855610f17fda79047f19db6730e60504a46e5bc0
Gabriel Portela Gomes <gabrielgomesportela@gmail.com>
2022-11-15 19:45:47 -0300

    feat: layout SignUpPage

--- src/pages/SignUpPage/SignUpPage.js
+++ src/pages/SignUpPage/SignUpPage.js
@@ -1,6 +1,78 @@
+import styled from "styled-components";
+
 function SignUpPage() {
-    return <></>;
+  return (
+    <PageContainer>
+      <Logo>MyWallet</Logo>
+      <SignUpForm>
+        <Input placeholder="Nome"></Input>
+        <Input placeholder="E-mail"></Input>
+        <Input placeholder="Senha"></Input>
+        <Input placeholder="Confirme a senha"></Input>
+        <Button>Cadastrar</Button>
+      </SignUpForm>
+      <Login>Já tem uma conta? Entre agora!</Login>
+    </PageContainer>
+  );
+}
+
+export default SignUpPage;
+
+const PageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Logo = styled.h1`
+  font-family: "Saira Stencil One", cursive;
+  font-weight: 400;
+  font-size: 32px;
+  color: #ffffff;
+`;
+
+const SignUpForm = styled.form`
+  width: 326px;
+  margin-top: 24px;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  height: 58px;
+  border-radius: 5px;
+  border: none;
+  background-color: #ffffff;
+  margin-bottom: 13px;
+  padding: 15px;
+  font-family: "Raleway", sans-serif;
+  font-weight: 400;
+  font-size: 20px;
+  color: #000000;
+  &:-webkit-autofill {
+    -webkit-box-shadow: 0 0 0px 1000px #ffffff inset !important;
+    -webkit-text-fill-color: #000000 !important;
   }
-  
-  export default SignUpPage;
-  
+`;
+
+const Button = styled.button`
+  width: 100%;
+  height: 46px;
+  border-radius: 5px;
+  border: none;
+  background-color: #a328d6;
+  font-family: "Raleway", sans-serif;
+  font-weight: 700;
+  font-size: 20px;
+  color: #ffffff;
+  cursor: pointer;
+`;
+
+const Login = styled.p`
+  font-family: "Raleway", sans-serif;
+  font-weight: 700;
+  font-size: 18px;
+  color: #ffffff;
+  margin-top: 36px;
+  cursor: pointer;
+`;
