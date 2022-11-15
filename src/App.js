a5e0f345bfae7dff4ff9e6c1c70f9dbd7b5513e9
Gabriel Portela Gomes <gabrielgomesportela@gmail.com>
2022-11-15 17:16:50 -0300

    feat: default screen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import styled from "styled-components";
 import GlobalStyle from "./assets/GlobalStyle";
 
 function App() {
   return (
     <div>
       <GlobalStyle />
+      <ScreenContainer></ScreenContainer>
     </div>
   );
 }
 
 export default App;
+
+const ScreenContainer = styled.main`
+  width: 375px;
+  height: 667px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  background-color: #8c11be; ;
+`;
